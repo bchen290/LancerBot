60e2b17c9160d14c21b545f1234763fd08aaff33
Brian Chen <brianchen0810@gmail.com>
2018-08-15 18:21:46 -0400

    Initial Dump

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='`')
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+
+with open('bot_token.txt') as bot_token_file:
+    token = bot_token_file.readline()
+    bot.run(token)
